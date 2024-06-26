26dc451e7ced327c3e96b848753019dccc72f8d5
Tahiry_Randrianarisolo <tahiry.rsl@gmail.com>
2024-06-26 23:49:29 +0200

    "Updated models, templates, and styles for file upload and login functionality."

--- project/file/models.py
+++ project/file/models.py
@@ -5,14 +5,14 @@ from django.db import models
 
 class File(models.Model):
     niveau_choix=[
-        ('CE1', 'Classe de CE1'),
-        ('CE2', 'Classe de CE2'),
-        ('CM1', 'Classe de CM1'),
-        ('CM2', 'Classe de CM2'),
-        ('6eme', 'Classe de 6ème'),
-        ('5eme', 'Classe de 5ème'),
-        ('4eme', 'Classe de 4ème'),
-        ('3eme', 'Classe de 3ème'),
+        ('CE1', 'CE1'),
+        ('CE2', 'CE2'),
+        ('CM1', 'CM1'),
+        ('CM2', 'CM2'),
+        ('6eme', ' 6ème'),
+        ('5eme', '5ème'),
+        ('4eme', '4ème'),
+        ('3eme', '3ème'),
     ]
     matiere_choix = [
         ('Maths', 'Mathematics'),
@@ -29,7 +29,8 @@ class File(models.Model):
     niveau = models.CharField(null=True,choices=niveau_choix)
     matiere = models.CharField(null=True,choices=matiere_choix)
     caption = models.CharField(max_length=100)
-    file = models.BinaryField(null=True, editable=True)
+    file = models.FileField(null=True, editable=True)
+   #file = models.BinaryField(null=True, editable=True)
     def __str__(self):
         return self.caption
 
